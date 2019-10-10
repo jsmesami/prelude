a1280d498f31a725fd1abba78e980e1fbffee25b
Ondra Nejedlý <jsmesami@gmail.com>
2019-10-10 20:27:34 +0200

    Configure parinfer and projectile

--- personal/personal.el
+++ personal/personal.el
@@ -2,13 +2,10 @@
 (when window-system
   (add-to-list 'default-frame-alist '(fullscreen . maximized)))
 
-
 ;; No scratch buffer message
 (setq initial-scratch-message nil)
 
 ;; Set font size
 (set-face-attribute 'default nil :height 120)
 
-;; Enable Paredit for Clojure
-(add-hook 'clojure-mode-hook #'paredit-mode)
 
