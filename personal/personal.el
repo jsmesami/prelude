8d033327cd789ff1a72bfb57f702d9b1f79b3980
Ondra Nejedlý <jsmesami@gmail.com>
2019-10-10 16:36:45 +0200

    Ditch Parinfer, enable Paredit

--- personal/personal.el
+++ personal/personal.el
@@ -6,3 +6,9 @@
 ;; No scratch buffer message
 (setq initial-scratch-message nil)
 
+;; Set font size
+(set-face-attribute 'default nil :height 120)
+
+;; Enable Paredit for Clojure
+(add-hook 'clojure-mode-hook #'paredit-mode)
+
