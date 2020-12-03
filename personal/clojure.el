6aaee8f07dd2ebafadd201f388c6fc83476e596b
Ondřej Nejedlý <jsmesami@gmail.com>
2020-12-03 01:06:53 +0100

    Add tab indenting

--- personal/clojure.el
+++ personal/clojure.el
@@ -1,12 +1,15 @@
 (prelude-require-packages '(parinfer))
 
-(global-set-key (kbd "C-,") 'parinfer-toggle-mode)
-
-(setq parinfer-extensions
-  '(defaults
-    pretty-parens))
+(setq parinfer-extensions '(defaults pretty-parens))
 
 (add-hook 'clojure-mode-hook #'cider-mode)
 
 (add-hook 'clojure-mode-hook #'parinfer-mode)
+
+(defun clojure-mode-keybindings ()
+  (local-set-key (kbd "C-,") 'parinfer-toggle-mode)
+  (local-set-key [tab] 'sp-indent-adjust-sexp)
+  (local-set-key [backtab] 'sp-dedent-adjust-sexp))
+
+(add-hook 'clojure-mode-hook #'clojure-mode-keybindings)
 
